49d5ba01c382460a87e2a2e8ac5dd788361eb676
davemurdock55 <davemurdock55@gmail.com>
2025-02-10 22:41:12 -0700

    first commit

---
+++ layers/commons/index.ts
@@ -0,0 +1,7 @@
+export const responseBuilder = (payload: any, statusCode: number = 200, headers: any = { "Content-Type": "application/json" }) => {
+  return {
+    statusCode,
+    headers,
+    body: JSON.stringify(payload),
+  };
+};
